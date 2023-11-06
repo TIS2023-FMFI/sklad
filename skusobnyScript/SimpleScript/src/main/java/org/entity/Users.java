a9956158710e1289a6eae439836f53d7e0103016
matpal7 <125296225+matpal7@users.noreply.github.com>
2023-11-07 00:08:31 +0100

    skusobny script

---
+++ skusobnyScript/SimpleScript/src/main/java/org/entity/Users.java
@@ -0,0 +1,71 @@
+package org.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Users {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "email")
+    private String email;
+
+    public Users() {
+    }
+    public Users(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Users users = (Users) o;
+
+        if (id != users.id) return false;
+        if (name != null ? !name.equals(users.name) : users.name != null) return false;
+        if (email != null ? !email.equals(users.email) : users.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
