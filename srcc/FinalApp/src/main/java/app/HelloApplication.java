3dbf4fd3fddf364ba419a2af7867e5c7988e293a
FPato <patrik.filipiak1@gmail.com>
2023-11-28 10:30:30 +0100

    zacatie triedy warehouse

--- srcc/FinalApp/src/main/java/app/HelloApplication.java
+++ srcc/FinalApp/src/main/java/app/HelloApplication.java
@@ -3,11 +3,16 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,7 +20,40 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }*/
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the X and Y axes
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        // Create the line chart
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Sample Line Chart");
+
+        // Create a data series
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Data Series");
+
+        // Add data points to the series
+        series.getData().add(new XYChart.Data<>(1, 5));
+        series.getData().add(new XYChart.Data<>(2, 10));
+        series.getData().add(new XYChart.Data<>(3, 15));
+        series.getData().add(new XYChart.Data<>(4, 20));
+
+        // Add the series to the chart
+        lineChart.getData().add(series);
+
+        // Create the scene and set it on the stage
+        Scene scene = new Scene(lineChart, 600, 400);
+        primaryStage.setScene(scene);
+
+        // Set the stage title and show it
+        primaryStage.setTitle("JavaFX Line Chart Example");
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
---
+++ srcc/FinalApp/src/main/java/app/Warehouse.java
@@ -0,0 +1,75 @@
+package app;
+
+import Entity.Material;
+import Entity.User;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class Warehouse extends Application{
+    //DatabaseHandler databaseHandler;
+    /***
+     * Aktuálne prihlásený používateľ.
+     */
+    User currentUser;
+
+    /***
+     * Mapa, ktorá obsahuje údaje o obsahu sklade. Kľúč prvej mapy je rad v sklade, kľúč druhej mapy
+     * je názov pozície a hodnota je zoznam obsahujúci dvojice materiálu a jeho počtu na tej pozícii.
+     */
+    Map<String,Map<String, List<Pair<Material, Integer>>>> warehouseData;
+
+    private static Stage stage;
+
+    /***
+     * Hlavná metóda, ktorá spustí aplikáciu.
+     * @param primarystage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primarystage) throws Exception {
+        //databaseHandler = new DatabaseHandler();
+        stage = primarystage;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("app/login.fxml")));
+        primarystage.setTitle("Skladovací systém");
+        primarystage.setScene(new Scene(root, 400, 300));
+        primarystage.show();
+    }
+
+    /***
+     * Metóda, ktorá načíta údaje z databázy do pamäte po úspešnom prihlásení.
+     */
+    public void loadDb(){
+        //warehouseData = DatabaseHandler.getWarehouseData();
+    }
+
+    /***
+     * Metóda, ktorá zmení scénu na základe fxml súboru.
+     * @param fxml Fxml súbor s novou scénou.
+     * @throws IOException Ak sa nepodarí načítať súbor.
+     */
+    public void changeScene(String fxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        Parent pane = loader.load();
+
+        double newWidth = pane.prefWidth(-1);
+        double newHeight = pane.prefHeight(-1);
+
+        stage.getScene().setRoot(pane);
+
+        stage.getScene().getWindow().setWidth(newWidth);
+        stage.getScene().getWindow().setHeight(newHeight);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
