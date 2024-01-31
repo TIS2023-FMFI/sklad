3329e5a1bbacc258ace93712aa829665152e6f5d
SasaSnidova <s.snidova@gmail.com>
2024-01-31 20:26:33 +0100

    vzhľad formulára orderShowPositionsForm

--- src/FinalApp/src/main/java/GUI/OrderProduct/OrderShowPositionsController.java
+++ src/FinalApp/src/main/java/GUI/OrderProduct/OrderShowPositionsController.java
@@ -10,8 +10,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.util.Pair;
 
@@ -42,15 +42,19 @@ public class OrderShowPositionsController implements Initializable {
 
         TableColumn<Map, String> materialColumn = new TableColumn<>("Materiál");
         materialColumn.setCellValueFactory(new MapValueFactory<>("Materiál"));
+        materialColumn.setPrefWidth(175);
 
         TableColumn<Map, String> quantityColumn = new TableColumn<>("Počet");
         quantityColumn.setCellValueFactory(new MapValueFactory<>("Počet"));
+        quantityColumn.setPrefWidth(74);
 
-        TableColumn<Map, String>positionColumn = new TableColumn<>("Pozícia");
+        TableColumn<Map, String> positionColumn = new TableColumn<>("Pozícia");
         positionColumn.setCellValueFactory(new MapValueFactory<>("Pozícia"));
+        positionColumn.setPrefWidth(175);
 
         TableColumn<Map, String> PNRColumn = new TableColumn<>("PNR");
         PNRColumn.setCellValueFactory(new MapValueFactory<>("PNR"));
+        PNRColumn.setPrefWidth(74);
 
         orderTable.getColumns().addAll(materialColumn, quantityColumn, positionColumn, PNRColumn);
 
